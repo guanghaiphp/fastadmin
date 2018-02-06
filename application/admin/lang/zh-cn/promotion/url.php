5fb5b97040268c0cbf6f3d66c2db191bb26c1c5e
value94 <x_x9304@163.com>
2018-02-06 11:02:35 +0800

    增加推广管理

---
+++ application/admin/lang/zh-cn/promotion/url.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'Id'  =>  '编号',
+    'User_id'  =>  '用户编号',
+    'Advert_id'  =>  '广告编号',
+    'Status'  =>  '状态',
+    'Promotion_url'  =>  '推广链接',
+    'Create_time'  =>  '创建时间',
+    'Update_time'  =>  '修改时间'
+];
